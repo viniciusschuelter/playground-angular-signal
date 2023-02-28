a9f55d5c20c133b4bd2663dc6b2a41b0abb71e2e
VInicius Schuelter <48526428+viniciusschuelter@users.noreply.github.com>
2023-02-28 19:58:15 -0300

    feat(dimension-c137): dimension c137 page in progress

---
+++ src/app/pages/dimension-c137/dimension-c137.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    standalone: true,
+    selector: 'app-dimension-c137',
+    template: `
+        <div class="container container-fluid">
+
+        </div>
+    `,
+})
+export class DimensionC137Component {
+    listItems: number[] = [...Array(100).keys()];
+}
